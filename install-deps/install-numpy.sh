8f62ef2d211ec59d62d38de483e12e69fc7d706e
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2008-01-25 00:52:47 +0000

    mcvine/install-deps: have to check in now due to svn limitation. the next check-in will complete this revision
    git-svn-id: svn://danse.us/buildInelast/mcvine@195 e9de5ee5-d905-4afb-841f-18304ef63348

--- install-deps/install-numpy.sh
+++
@@ -1,8 +0,0 @@
-  export PREF=$HOME/software
-  # http://superb-east.dl.sourceforge.net/sourceforge/numpy/numpy-1.0b1.tar.gz
-  wget http://superb-east.dl.sourceforge.net/sourceforge/numpy/numpy-1.0b1.tar.gz
-  tar zxvf numpy-1.0b1.tar.gz
-  cd numpy-1.0b1
-  python setup.py build
-  python setup.py install
-  cd ..
