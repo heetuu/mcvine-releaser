4768f5745ea603a09dfced078d2e7d97bbb2d34b
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2008-05-21 17:38:07 +0000

    use 'py_numpy' to identify the numpy package to reduce the danger of name conflict in python imports
    git-svn-id: svn://danse.us/buildInelast/mcvine@270 e9de5ee5-d905-4afb-841f-18304ef63348

--- deps.py
+++ deps.py
@@ -1,12 +1,12 @@
 
 packages = [
-    'numpy',
+    'py_numpy',
     'boostpython',
     #'McStas',
     ]
 
 descriptions = {
-    'numpy': "Numerical Python. see http://numpy.scipy.org/",
+    'py_numpy': "Numerical Python. see http://numpy.scipy.org/",
     'boostpython': 'Boostpython',
     #'McStas': 'Risoe McStas',
     }
@@ -22,7 +22,7 @@ installers = {}
 
 from utils.installers import get as get_installer
 
-installers[ 'numpy' ] = get_installer( 'numpy', 'src' )
+installers[ 'py_numpy' ] = get_installer( 'py_numpy', 'src' )
 installers[ 'boostpython' ] = get_installer( 'boostpython', 'src' )
 #installers[ 'McStas' ] = get_installer( 'McStas', 'src' )
 
