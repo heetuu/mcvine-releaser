923a969efde96434e6dd84e72906bba7b7aac865
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2009-06-26 16:23:18 +0000

    mcvine/packages/config.py: minor
    git-svn-id: svn://danse.us/buildInelast/mcvine@324 e9de5ee5-d905-4afb-841f-18304ef63348

--- packages/config.py
+++ packages/config.py
@@ -4,5 +4,5 @@ deps = []
 from utils import repoutils
 reponame = 'ctrl'
 branch = "config/branches/with_doxygen_and_docbook_support"
-path, checkoutCmd, updateCmd = repoutils.svn.repoinfo( reponame, branch )
+path, checkoutCmd, updateCmd = repoutils.svn.repoinfo( reponame, branch, name=name)
 
