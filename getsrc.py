13b19ab8125b2821bdb0705b69271a6e6b4f5fc7
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2010-05-19 03:20:38 +0000

    mcvine/getsrc.py: now can specify the packages to get
    git-svn-id: svn://danse.us/buildInelast/mcvine@438 e9de5ee5-d905-4afb-841f-18304ef63348

--- getsrc.py
+++ getsrc.py
@@ -14,14 +14,23 @@
 
 # script to check out sources
 
+from packages import packageNames, checkout
 
-def main():
-    from packages import packageNames, checkout
+def get(names):
     from directory_structure import tree
     srcRt = tree.search( "sources" ).path
-    checkout(packageNames, srcRt)
+    if not names: names = packageNames
+    checkout(names, srcRt)
     #import dereference
     #dereference.dereference_recursively( 'src' )
+    return
+
+
+def main():
+    import sys
+    argv = sys.argv
+    names = argv[1:]
+    get(names)
     return
 
 
