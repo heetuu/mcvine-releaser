dd4c69674214e134b57cd62f2bcc3b55d69a126b
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2010-11-03 01:09:34 +0000

    mcvine/build.py: simplified by using utils.scripts.build
    git-svn-id: svn://danse.us/buildInelast/mcvine@524 e9de5ee5-d905-4afb-841f-18304ef63348

--- build.py
+++ build.py
@@ -12,52 +12,7 @@
 #
 
 
-def build(export_root):
-    import sys, os
-    pwd = os.path.abspath( os.curdir )
-
-    import sys
-    sys.path = [pwd] + sys.path
-
-    from utils.build import build_release, clean_up
-    build_release( pwd, export_root=export_root)
-    return
-
-
-def main():
-    import sys, os, shlex
-
-    from utils.datastore import open
-    build_info = open('build_info')
-    
-    if len(sys.argv) == 2:
-        export_root = sys.argv[1]
-    else:
-        if build_info.get('export_root'):
-            export_root = build_info['export_root']
-        else:
-            export_root = os.path.abspath('EXPORT')
-
-    build_info['export_root'] = export_root
-    
-    cmd = 'python -c "import build; build.build(%r)"' % export_root
-    args = shlex.split(cmd)
-
-    deps_root = os.path.join(export_root, 'deps')
-    env = os.environ.copy()
-    env['PATH'] = '%s:%s' % (
-        os.path.join( deps_root, 'bin' ), env['PATH'] )
-    env['LD_LIBRARY_PATH'] = '%s:%s' % (
-        os.path.join( deps_root, 'lib' ), env.get('LD_LIBRARY_PATH') or '' )
-    env['DYLD_LIBRARY_PATH'] = '%s:%s' % (
-        os.path.join( deps_root, 'lib' ), env.get('DYLD_LIBRARY_PATH') or '' )
-    env['PYTHONPATH'] = '%s:%s' % (
-        os.path.join( deps_root, 'python' ), env.get('PYTHONPATH', '') )
-    import subprocess
-    p = subprocess.Popen(args, env=env)
-    while p.poll() is None:
-        p.communicate()
-
+from utils.scripts.build import main
 
 if __name__ == '__main__': main()
 
