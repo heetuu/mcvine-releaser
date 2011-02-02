4c7ce9a0b2ff30f972bc1b48ebdca97b3cebb397
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2011-02-02 18:22:00 +0000

    mcvine/deps.py: added "psutil" to the dependency list
    git-svn-id: svn://danse.us/buildInelast/mcvine@572 e9de5ee5-d905-4afb-841f-18304ef63348

--- deps.py
+++ deps.py
@@ -1,6 +1,7 @@
 
 packages = [
     'py_numpy',
+    'py_psutil',
     'boostpython',
     'py_h5py',
     'mpich2',
@@ -10,6 +11,7 @@ packages = [
 
 descriptions = {
     'py_numpy': "Numerical Python. see http://numpy.scipy.org/",
+    'py_psutil': 'psutil python module',
     'boostpython': 'Boostpython',
     'py_h5py': 'python binding of hdf5',
     'mpich2': 'mpich2',
@@ -29,6 +31,7 @@ installers = {}
 from utils.installers import get as get_installer
 
 installers[ 'py_numpy' ] = get_installer( 'py_numpy', 'src' )
+installers[ 'py_psutil' ] = get_installer( 'py_psutil', 'src')
 installers[ 'boostpython' ] = get_installer( 'boostpython', 'src' )
 installers[ 'py_h5py' ] = get_installer( 'py_h5py', 'src' )
 installers[ 'mpich2' ] = get_installer( 'mpich2', 'src' )
