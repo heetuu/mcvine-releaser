84fee860faaea5471dff2740eb71b8c7483de2cd
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2011-02-26 15:54:06 +0000

    mcvine/buildbot/test-and-build-dists.py: minor tweak
    git-svn-id: svn://danse.us/buildInelast/mcvine@585 e9de5ee5-d905-4afb-841f-18304ef63348

--- buildbot/test-and-build-dists.py
+++ buildbot/test-and-build-dists.py
@@ -5,12 +5,12 @@
 # this script must be run from the root of the mcvine releaser
 
 def main():
-    import test
-    failed = test.runall()
-    if failed:
-        return
-
     import os
+    cmd = './test.py'
+    if os.system(cmd):
+        import sys
+        sys.exit(1)
+
     cmd = './create-src-dist-snapshot.py'
     if os.system(cmd):
         raise RuntimeError, "%s failed"
