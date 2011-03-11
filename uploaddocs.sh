551ce90ce9a47efd542d91d6dddae91c5351b5fb
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2011-03-11 20:58:19 +0000

    mcvine/uploaddocs.sh: upload example tar ball as well
    git-svn-id: svn://danse.us/buildInelast/mcvine@592 e9de5ee5-d905-4afb-841f-18304ef63348

--- uploaddocs.sh
+++ uploaddocs.sh
@@ -1,2 +1,3 @@
 chmod -R a+rX EXPORT/docs/mcvine/sphinx/
 rsync -r EXPORT/docs/mcvine/sphinx/ linjiao@login.cacr.caltech.edu:docs.danse.us-docroot/MCViNE/sphinx/
+scp EXPORT/docs/mcvine/sphinx/examples.tgz linjiao@login.cacr.caltech.edu:projects/danse/packages/dev_danse_us/mcvine-examples.tgz
