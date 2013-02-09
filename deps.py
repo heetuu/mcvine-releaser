039cfcbe79c0f2ecbacb472ee6e20633af2c2af0
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2013-02-09 00:36:04 +0000

    deps.py: HDF5 is a dependency
    git-svn-id: svn://danse.us/buildInelast/mcvine@639 e9de5ee5-d905-4afb-841f-18304ef63348

--- deps.py
+++ deps.py
@@ -3,6 +3,7 @@ packages = [
     'py_numpy',
     'py_psutil',
     'boostpython',
+    'HDF5',
     'py_h5py',
     'mpich2',
     #'wxPython',
@@ -13,6 +14,7 @@ descriptions = {
     'py_numpy': "Numerical Python. see http://numpy.scipy.org/",
     'py_psutil': 'psutil python module',
     'boostpython': 'Boostpython',
+    'HDF5': 'HDF5',
     'py_h5py': 'python binding of hdf5',
     'mpich2': 'mpich2',
     'McStas': 'Risoe McStas',
@@ -33,6 +35,7 @@ from utils.installers import get as get_installer
 installers[ 'py_numpy' ] = get_installer( 'py_numpy', 'src' )
 installers[ 'py_psutil' ] = get_installer( 'py_psutil', 'src')
 installers[ 'boostpython' ] = get_installer( 'boostpython', 'src' )
+installers[ 'HDF5' ] = get_installer( 'HDF5', 'src' )
 installers[ 'py_h5py' ] = get_installer( 'py_h5py', 'src' )
 installers[ 'mpich2' ] = get_installer( 'mpich2', 'src' )
 installers[ 'wxPython' ] = get_installer( 'wxPython', 'src' )
