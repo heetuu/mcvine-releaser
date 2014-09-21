75ace0938e57234f340043ac29ceb42b817fe50b
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2014-09-21 18:29:46 +0000

    mcvine-releaser/packages/histogram.py: switch to git
    git-svn-id: svn://danse.us/buildInelast/mcvine@649 e9de5ee5-d905-4afb-841f-18304ef63348

--- packages/histogram.py
+++ packages/histogram.py
@@ -1,4 +1,4 @@
 from utils.packages.histogram import *
 
 from utils.package import repoutils
-repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+repo = repoutils.git.getPackageRepository(reponame, branch, name=name)
