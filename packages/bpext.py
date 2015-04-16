6576707265ed14fa35b2ea94b03138bc13b15a6e
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2015-04-16 15:03:07 +0000

    packages/bpext.py: changed to git
    git-svn-id: svn://danse.us/buildInelast/mcvine@661 e9de5ee5-d905-4afb-841f-18304ef63348

--- packages/bpext.py
+++ packages/bpext.py
@@ -1,4 +1,4 @@
 from utils.packages.bpext import *
 
 from utils.package import repoutils
-repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+repo = repoutils.git.getPackageRepository(reponame, branch, name=name)
