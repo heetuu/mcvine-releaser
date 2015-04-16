d07e78c655d8970d57c0183f4216be26dcdda71a
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2015-04-16 15:29:48 +0000

    packages/config.py: switch to git
    git-svn-id: svn://danse.us/buildInelast/mcvine@663 e9de5ee5-d905-4afb-841f-18304ef63348

--- packages/config.py
+++ packages/config.py
@@ -1,7 +1,12 @@
-name = 'config'
-deps = []
+# name = 'config'
+# deps = []
+
+# from utils.package import repoutils
+# reponame = 'ctrl'
+# branch = "config/branches/with_doxygen_and_docbook_support"
+# repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+
+from utils.packages.config import *
 
 from utils.package import repoutils
-reponame = 'ctrl'
-branch = "config/branches/with_doxygen_and_docbook_support"
-repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+repo = repoutils.git.getPackageRepository(reponame, branch, name=name)
