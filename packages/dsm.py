deb2eee73715e6d29f7792c54d991c14431cdbc1
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2015-04-17 16:40:10 +0000

    dsm.py: switch to git
    git-svn-id: svn://danse.us/buildInelast/mcvine@667 e9de5ee5-d905-4afb-841f-18304ef63348

--- packages/dsm.py
+++ packages/dsm.py
@@ -1,4 +1,10 @@
 from utils.packages.dsm import *
-
 from utils.package import repoutils
-repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+
+# old: svn
+# repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+
+# git
+reponame = "dsm"
+branch = "master"
+repo = repoutils.git.getPackageRepository(reponame, branch, name=name)
