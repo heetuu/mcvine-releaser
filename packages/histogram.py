5f57ec680e95f3454d10239d33ffd6870e0d7b4a
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2015-04-17 18:41:05 +0000

    packages/histogram.py: minor -- added py_numpy to deps
    git-svn-id: svn://danse.us/buildInelast/mcvine@668 e9de5ee5-d905-4afb-841f-18304ef63348

--- packages/histogram.py
+++ packages/histogram.py
@@ -1,5 +1,5 @@
 from utils.packages.histogram import *
-deps = () # histogram only depends on numpy now
+deps = ('py_numpy',) # histogram only depends on numpy now
 
 from utils.package import repoutils
 repo = repoutils.git.getPackageRepository(reponame, branch, name=name)
