1cca87134b6242312497cf980269d9484881ce9b
linjiao <linjiao@e9de5ee5-d905-4afb-841f-18304ef63348>
2015-04-18 00:26:47 +0000

    packages/sampleassembly.py,sansmodels.py: switch to git
    git-svn-id: svn://danse.us/buildInelast/mcvine@671 e9de5ee5-d905-4afb-841f-18304ef63348

--- packages/sampleassembly.py
+++ packages/sampleassembly.py
@@ -1,4 +1,10 @@
 from utils.packages.sampleassembly import *
 
 from utils.package import repoutils
-repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+# svn
+# repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+
+# git
+reponame = "sampleassembly"
+branch = "master"
+repo = repoutils.git.getPackageRepository(reponame, branch, name=name)
--- packages/sansmodels.py
+++ packages/sansmodels.py
@@ -1,4 +1,11 @@
 from utils.packages.sansmodels import *
 
 from utils.package import repoutils
+
+# old: svn
+# repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+
+# git
+reponame = "sansmodels"
+branch = "master"
 repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
