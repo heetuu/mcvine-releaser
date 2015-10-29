d4b5aa0be1ef670b2e37460236308ac90a154939
Jiao Lin <linjiao@ornl.gov>
2015-10-29 13:41:38 -0400

    removed "dsm" from packages to build. it becomes a dependency

--- packages/obsolete/dsm.py
+++ packages/obsolete/dsm.py
@@ -1,1 +1,10 @@
-../../common/packages/dsm.py
+from utils.packages.dsm import *
+from utils.package import repoutils
+
+# old: svn
+# repo = repoutils.svn.getPackageRepository(reponame, branch, name=name)
+
+# git
+reponame = "dsm"
+branch = "master"
+repo = repoutils.git.getPackageRepository(reponame, branch, name=name)
