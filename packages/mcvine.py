9d5552b51b354e6dbd736f674b224789e8dfef4f
Jiao Lin <linjiao@caltech.edu>
2015-11-11 19:11:25 -0800

    packages/mcvine.py: revert to master branch

--- packages/mcvine.py
+++ packages/mcvine.py
@@ -7,6 +7,7 @@ from utils.package import repoutils
 
 # git
 reponame = "mcvine"
-branch = "11-mv-danse.ins-deps-out"
+# branch = "11-mv-danse.ins-deps-out"
+branch = "master"
 server = "https://github.com/mcvine"
 repo = repoutils.git.getPackageRepository(reponame, branch, name=name, server=server)
